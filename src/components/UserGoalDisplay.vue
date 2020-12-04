d8879784b90ff4491854f421fb47bc7f94c410ac
Joshua <joshbelow@gmail.com>
2020-12-04 13:47:25 -0600

    the template is starting to look a little better ish

--- src/components/UserGoalDisplay.vue
+++ src/components/UserGoalDisplay.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <div class="goalStyle py-5 mx-lg-auto" v-for="g in goals" :key="g.id">
-      <v-card class="blue darken-2" id="goal-display">
-        <v-card-title class="headline white--text funFont">
-          <h2>
+      <v-card class="blue darken-2" id="goal-display" outlined>
+        <v-card-title class="white--text">
+          <h3>
             {{ g.title }}
-          </h2>
+          </h3>
           <v-spacer></v-spacer>
-
           <!-- options/menu -->
           <v-menu bottom left>
             <template v-slot:activator="{ on, attrs }">
@@ -15,7 +14,6 @@
                 <v-icon>mdi-dots-vertical</v-icon>
               </v-btn>
             </template>
-
             <v-list>
               <v-list-item link>
                 <!-- --------------------------------- -->
@@ -90,42 +88,22 @@
             </v-list>
           </v-menu>
           <!--  options/menu end-->
+          <v-list-item>
+            <v-list-item-content class=" white--text">
+              Progress: {{ g.minutesProgress }}/{{ g.minutes }} minutes
+              <br />
+              Difficulty: {{ g.difficulty }}
+              <br />
+              Details: {{ g.details }}
+            </v-list-item-content>
+          </v-list-item>
         </v-card-title>
 
-        <v-card-subtitle>
-
-          {{ g.minutesProgress }}/{{ g.minutes }}
-
-          {{ g.details }} - goal time: {{ g.minutes }}</v-card-subtitle
-        >
+        <v-card-actions>
+          <v-btn> Add Time </v-btn>
+        </v-card-actions>
       </v-card>
     </div>
-<!-- --------------------------------------------------------------------------------------------------- -->
-    <v-card class="mx-auto" max-width="344" outlined>
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">OVERLINE</div>
-          <v-list-item-title class="headline mb-1">
-            Headline 5
-          </v-list-item-title>
-          <v-progress-linear
-            color="light-blue"
-            height="10"
-            value="10"
-            striped
-          ></v-progress-linear>
-          <v-list-item-subtitle
-            >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
-          >
-        </v-list-item-content>
-
-        <v-list-item-avatar tile size="80" color="grey">test</v-list-item-avatar>
-      </v-list-item>
-
-      <v-card-actions>
-        <v-btn outlined rounded text> Button </v-btn>
-      </v-card-actions>
-    </v-card>
   </div>
 </template>
 
@@ -157,8 +135,10 @@ export default {
               goal.details = doc.data().goalDetails;
               goal.minutesProgress = doc.data().goalMinutesProgress;
               goal.minutes = doc.data().goalMinutes;
-              // console.log(goal);
-              this.goals.push(goal);
+              goal.difficulty = doc.data().goalDifficulty;
+              (goal.progress = goal.minutesProgress / goal.minutes),
+                // console.log(goal);
+                this.goals.push(goal);
             });
           });
       }
