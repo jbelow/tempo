2237df7c0988e6cb086355fb1471dcb80381d544
Joshua <joshbelow@gmail.com>
2020-12-06 17:39:03 -0600

    log now has the date as I want it

--- src/components/UserGoalDisplayBtnViewLog.vue
+++ src/components/UserGoalDisplayBtnViewLog.vue
@@ -8,15 +8,13 @@
     <v-card>
       <v-card-text>
         <!-- <div class="font-weight-bold ml-8 mb-2">{{ GoalTitle }}</div> -->
-        <div>List of logs for {{this.goalTitle}}</div>
+        <div>List of logs for {{ this.goalTitle }}</div>
 
         <v-timeline align-top dense>
           <v-timeline-item v-for="l in logs" :key="l.id" small>
             <div>
               <div class="font-weight-normal">
-                <div>{{ l.LoggedDate }}</div>
-                <strong>{{ l.loggedDate }}</strong> {{ l.loggedMinutes }}
-                <div>test {{ l.loggedMinutes }}</div>
+                {{ moment(l.LoggedDate).format('MMM Do YYYY') }} <br> you logged: {{ l.loggedMinutes }} minutes
               </div>
               <!-- <div>{{ log.message }}</div> -->
             </div>
@@ -29,6 +27,7 @@
 
 <script>
 import firebase from "firebase";
+import moment from "moment";
 
 export default {
   props: ["goalId", "goalTitle"],
@@ -37,26 +36,6 @@ export default {
     return {
       dialog: false,
       logs: [],
-      messages: [
-        {
-          from: "You",
-          message: `Sure, I'll see you later.`,
-          time: "10:42am",
-          color: "deep-purple lighten-1",
-        },
-        {
-          from: "John Doe",
-          message: "Yeah, sure. Does 1:00pm work?",
-          time: "10:37am",
-          color: "green",
-        },
-        {
-          from: "You",
-          message: "Did you still want to grab lunch today?",
-          time: "9:47am",
-          color: "deep-purple lighten-1",
-        },
-      ],
     };
   },
 
@@ -80,11 +59,17 @@ export default {
               log.loggedMinutes = doc.data().goalLoggedMinutes;
               log.LoggedDate = doc.data().goalLoggedDate;
               this.logs.push(log);
-            //   console.log(log.LoggedDate);
+              //   console.log(log.LoggedDate);
             });
           });
       }
     });
+  },
+
+  methods: {
+    moment: function (date) {
+      return moment(date);
+    },
   },
 };
 </script>
